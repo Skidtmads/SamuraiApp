f3d160835c6e13ebd308f868ccf5b1937ef31176
Christian Brandt Henriksen <christian_brandt_henriksen@hotmail.com>
2023-01-12 21:42:16 +0100

    21:45

---
+++ Pages/Armortype.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Samurai.Pages
+{
+    public class ArmortypeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
